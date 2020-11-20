d7d3f48de8cc45e1fa38e82cc686944a6e6e5d03
Erik Svensson <zesdev@gmail.com>
2020-11-20 15:23:14 +0100

    final fredag

--- BildStudionDV.Web/Controllers/WikiController.cs
+++ BildStudionDV.Web/Controllers/WikiController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,28 @@ namespace BildStudionDV.Web.Controllers
 {
     public class WikiController : Controller
     {
+        [Authorize]
         public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Konton()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Inventarier()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult KundJobb()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Närvaro()
         {
             return View();
         }
